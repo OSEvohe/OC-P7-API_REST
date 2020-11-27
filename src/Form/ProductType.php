8c291d5cc90525dbf822de22858cac6447a544d9
Sebastien <sebastien@o-pa.fr>
2020-11-27 22:59:28 +0100

    Product form Type

---
+++ src/Form/ProductType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('image')
+            ->add('brand')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+            'allow_extra_fields' => true,
+            'csrf_protection'    => false,
+        ]);
+    }
+}
